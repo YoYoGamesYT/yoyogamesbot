b8c6aed605d33f2d33ee8706210d7abb0a0958e3
YoYoGames <35001431+YoYoGamesYT@users.noreply.github.com>
2018-01-01 13:50:13 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("YoYoBot, y!help");
+    console.log("Le bot a bien été connecté");
+});
+
+bot.login("Mzk1NjQ3NjYyODQ3ODE5Nzg2.DSu7kQ.oSPvfi2rVC_T_MWrcf28KSfP6Js");
